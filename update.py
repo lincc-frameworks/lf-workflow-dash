925d85b7972f371aab1eabd1613badd3729c50ca
Olivia R. Lynn <olynn@andrew.cmu.edu>
2023-09-23 11:59:22 -0400

    Update update.py

--- update.py
+++ update.py
@@ -1,12 +1,14 @@
 import requests
+import sys
 
 if __name__ == "__main__":
+    token = sys.argv[1]
     url = "https://api.github.com/repos/lincc-frameworks/tape/actions/runs"
     
     payload = {}
     headers = {
       'accept': 'application/vnd.github+json',
-      'Authorization': 'Bearer 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'
+      'Authorization': f'Bearer {token}'
     }
     
     response = requests.request("GET", url, headers=headers, data=payload)
