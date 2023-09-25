e9d1e6d41e1d81af9611b1266b6ae0b296787f83
Olivia Lynn <olynn@andrew.cmu.edu>
2023-09-25 09:16:59 -0400

    table display, timezones

--- update.py
+++ update.py
@@ -1,10 +1,11 @@
 from datetime import datetime
+import pytz
 import requests
 import sys
 
 
 class WorkflowData:
-    def __init__(self, token, owner, repo, workflow):
+    def __init__(self, token, owner, repo, workflow, tz=""):
         self.token = token
         self.owner = owner
         self.repo = repo
@@ -23,6 +24,10 @@ class WorkflowData:
             self.conclusion = last_run["conclusion"]
             self.updated_at = last_run["updated_at"]
 
+            if tz:
+                utc_timestamp = datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%SZ")
+                self.updated_at = utc_timestamp.astimezone(tz).strftime("%H:%M %b %d, %Y")
+
     def __str__(self):
         if self.status_code == 200:
             icon = "⚠"
@@ -33,9 +38,21 @@ class WorkflowData:
             icon = "⚠"
             return f"{icon} {self.repo} → {self.workflow}: bad api call"
 
+    def as_row(self):
+        if self.status_code == 200:
+            icon = "**⚠**"
+            if self.conclusion == "success":
+                icon = "✓"
+            return f"| {icon} | {self.repo} | {self.workflow} | {self.conclusion} | {self.updated_at} |"
+        else:
+            icon = "**⚠**"
+            return f"| {icon} | {self.repo} | {self.workflow} | bad api call | --- |"
+
 
 if __name__ == "__main__":
     token = sys.argv[1]
+
+    tz = pytz.timezone("America/New_York")
 
     file_name = "README.md"
     with open(file_name, "w") as file_out:
@@ -48,37 +65,34 @@ if __name__ == "__main__":
             file_out.write(str(WorkflowData(token, owner, repo, workflow)))
             file_out.write("\n\n")
 
-        add_text(f"Last Updated (UTC) {datetime.now().strftime('%b %d, %Y %H:%M')}")
-        #add_text(
-        #    "API reference: [list-workflow-runs-for-a-workflow](https://docs.github.com/en/rest/actions/workflow-runs?apiVersion=2022-11-28#list-workflow-runs-for-a-workflow)"
-        #)
-        #add_text("repo_name → workflow_name: workflow_conclusion (last_run_at)")
+        def add_row(owner, repo, workflow):
+            file_out.write(WorkflowData(token, owner, repo, workflow, tz=tz).as_row())
+            file_out.write("\n")
 
-        add_text("## WD")
-        add_workflow("OliviaLynn", "workflow-dash", "always-fails.yml")
-        add_workflow(
-            "OliviaLynn", "workflow-dash", "70388660"
-        )  # id for pages-build-deployment -> in the future would like to display name in output instead
+        add_text(f"Last Updated: {datetime.now(tz).strftime('%H:%M %b %d, %Y')}")
 
-        add_text("## LSDB")
-        add_workflow("astronomy-commons", "lsdb", "smoke-test.yml")
-        add_workflow("astronomy-commons", "lsdb", "testing-and-coverage.yml")
-        add_workflow("astronomy-commons", "lsdb", "asv-nightly.yml")
-        add_workflow("astronomy-commons", "lsdb", "build-documentation.yml")
+        file_out.write("| ? | repo | workflow | conclusion | updated at |\n")
+        file_out.write("| - | ---- | -------- | ---------- | ---------- |\n")
 
-        add_text("## HIPSCAT")
-        add_workflow("astronomy-commons", "hipscat", "smoke-test.yml")
-        add_workflow("astronomy-commons", "hipscat", "testing-and-coverage.yml")
-        add_workflow("astronomy-commons", "hipscat", "asv-nightly.yml")
-        add_workflow("astronomy-commons", "hipscat", "build-documentation.yml")
-        
-        add_text("## HIPSCAT-IMPORT")
-        add_workflow("astronomy-commons", "hipscat-import", "smoke-test.yml")
-        add_workflow("astronomy-commons", "hipscat-import", "testing-and-coverage.yml")
-        add_workflow("astronomy-commons", "hipscat-import", "asv-nightly.yml")
-        add_workflow("astronomy-commons", "hipscat-import", "build-documentation.yml")
+        add_row("OliviaLynn", "workflow-dash", "always-fails.yml")
+        add_row("OliviaLynn", "workflow-dash", "70388660")
+        # ^ workflow id for pages-build-deployment -> in the future would like to display name even when given id
 
-        add_text("## TAPE")
-        add_workflow("lincc-frameworks", "tape", "build-documentation.yml")
-        add_workflow("lincc-frameworks", "tape", "smoke-test.yml")
-        add_workflow("lincc-frameworks", "tape", "testing-and-coverage.yml")
+        add_row("astronomy-commons", "lsdb", "smoke-test.yml")
+        add_row("astronomy-commons", "lsdb", "testing-and-coverage.yml")
+        add_row("astronomy-commons", "lsdb", "asv-nightly.yml")
+        add_row("astronomy-commons", "lsdb", "build-documentation.yml")
+
+        add_row("astronomy-commons", "hipscat", "smoke-test.yml")
+        add_row("astronomy-commons", "hipscat", "testing-and-coverage.yml")
+        add_row("astronomy-commons", "hipscat", "asv-nightly.yml")
+        add_row("astronomy-commons", "hipscat", "build-documentation.yml")
+
+        add_row("astronomy-commons", "hipscat-import", "smoke-test.yml")
+        add_row("astronomy-commons", "hipscat-import", "testing-and-coverage.yml")
+        add_row("astronomy-commons", "hipscat-import", "asv-nightly.yml")
+        add_row("astronomy-commons", "hipscat-import", "build-documentation.yml")
+
+        add_row("lincc-frameworks", "tape", "build-documentation.yml")
+        add_row("lincc-frameworks", "tape", "smoke-test.yml")
+        add_row("lincc-frameworks", "tape", "testing-and-coverage.yml")
