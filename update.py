2f2a302a47092dba29cd23aabbae6dd571d6a111
Olivia Lynn <olynn@andrew.cmu.edu>
2023-10-01 19:03:24 -0400

    Switch to html build

--- update.py
+++ update.py
@@ -11,7 +11,7 @@ class WorkflowData:
         self.owner = owner
         self.repo = repo
         self.workflow = workflow
-        self.icon = "**⚠**"
+        self.icon = "⚠"
 
         self.url = f"https://github.com/{owner}/{repo}/actions/workflows/{workflow}"
         print(self.url)
@@ -36,6 +36,32 @@ class WorkflowData:
                 utc_timestamp = datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%SZ")
                 self.updated_at = utc_timestamp.astimezone(tz).strftime("%H:%M %b %d, %Y")
 
+    def html_row(self):
+        icon_color = 'class="red-icon"'
+        if self.status_code == 200 and self.conclusion == "success":
+            icon_color = 'class="green-icon"'
+
+        if self.status_code == 200:
+            return (
+                f"<tr>"
+                f"<td {icon_color}>{self.icon}</td>"
+                f"<td>{self.repo}</td>"
+                f'<td><a href="{self.url}">{self.workflow}</a></td>'
+                f"<td>{self.conclusion}</td>"
+                f"<td>{self.updated_at}</td>"
+                f"</tr>"
+            )
+        else:
+            return (
+                f"<tr>"
+                f"<td {icon_color}>{self.icon}</td>"
+                f"<td>{self.repo}</td>"
+                f'<td><a href="{self.url}">{self.workflow}</a></td>'
+                f"<td>{self.status_code}</td>"
+                f"<td></td>"
+                f"</tr>"
+            )
+
     def __str__(self):
         workflow_cell = self.workflow
         if self.url:
@@ -46,9 +72,7 @@ class WorkflowData:
             return f"| {self.icon} | {self.repo} | {workflow_cell} | bad api call | --- |"
 
 
-if __name__ == "__main__":
-    token = sys.argv[1]
-    tz = pytz.timezone("America/New_York")
+def update_readme(token, tz):
     file_name = "README.md"
 
     with open(file_name, "w") as file_out:
@@ -65,10 +89,6 @@ if __name__ == "__main__":
 
         file_out.write("| ? | repo | workflow | conclusion | updated at |\n")
         file_out.write("| - | ---- | -------- | ---------- | ---------- |\n")
-
-        # add_row("OliviaLynn", "workflow-dash", "always-fails.yml")
-        # add_row("OliviaLynn", "workflow-dash", "70388660")
-        # ^ workflow id for pages-build-deployment -> in the future would like to display name even when given id
 
         add_row("astronomy-commons", "lsdb", "smoke-test.yml")
         add_row("astronomy-commons", "lsdb", "testing-and-coverage.yml")
@@ -87,3 +107,96 @@ if __name__ == "__main__":
         add_row("lincc-frameworks", "tape", "build-documentation.yml")
         add_row("lincc-frameworks", "tape", "smoke-test.yml")
         add_row("lincc-frameworks", "tape", "testing-and-coverage.yml")
+
+
+def update_html(token, tz):
+    file_name = "index.html"
+
+    with open(file_name, "w") as file_out:
+
+        def add_text(line):
+            file_out.write(line)
+            file_out.write("\n\n")
+
+        def add_row(owner, repo, workflow):
+            file_out.write(WorkflowData(token, owner, repo, workflow, tz=tz).html_row())
+            file_out.write("\n")
+
+        # Write preamble
+        html_preamble = """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <title>LF Workflow Dashboard</title>
+                <link rel="stylesheet" href="style.css">
+            </head>
+            <body>
+            <table>
+            <tr>
+                <th> </th>
+                <th>Repository</th>
+                <th>Workflow</th>
+                <th>Conclusion</th>
+                <th>Last Run</th>
+            </tr>
+            """
+        file_out.write(html_preamble)
+
+        # Write middle
+
+        # ASV Formatter
+        add_row("lincc-frameworks", "asv-formatter", "smoke-test.yml")
+        add_row("lincc-frameworks", "asv-formatter", "testing-and-coverage.yml")
+
+        # Hipscat
+        add_row("astronomy-commons", "hipscat", "smoke-test.yml")
+        add_row("astronomy-commons", "hipscat", "testing-and-coverage.yml")
+        add_row("astronomy-commons", "hipscat", "asv-nightly.yml")
+        add_row("astronomy-commons", "hipscat", "build-documentation.yml")
+
+        # Hipscat Import
+        add_row("astronomy-commons", "hipscat-import", "smoke-test.yml")
+        add_row("astronomy-commons", "hipscat-import", "testing-and-coverage.yml")
+        add_row("astronomy-commons", "hipscat-import", "build-documentation.yml")
+
+        # Koffi
+        add_row("lincc-frameworks", "koffi", "smoke-test.yml")
+        add_row("lincc-frameworks", "koffi", "testing-and-coverage.yml")
+
+        # LSDB
+        add_row("astronomy-commons", "lsdb", "smoke-test.yml")
+        add_row("astronomy-commons", "lsdb", "testing-and-coverage.yml")
+        add_row("astronomy-commons", "lsdb", "asv-nightly.yml")
+        add_row("astronomy-commons", "lsdb", "build-documentation.yml")
+
+        # PPT
+        add_row("lincc-frameworks", "python-project-template", "ci.yml")
+
+        # Rail
+        add_row("lsstdesc", "rail", "build_documentation.yml")
+        add_row("lsstdesc", "rail", "smoke-test.yml")
+        add_row("lsstdesc", "rail", "testing-and-coverage.yml")
+        add_row("lsstdesc", "rail_base", "smoke-test.yml")
+        add_row("lsstdesc", "rail_base", "testing-and-coverage.yml")
+        add_row("lsstdesc", "rail_pipelines", "main.yml")
+
+        # Tables IO
+        add_row("lsstdesc", "tables_io", "smoke-test.yml")
+        add_row("lsstdesc", "tables_io", "testing-and-coverage.yml")
+
+        # Tape
+        add_row("lincc-frameworks", "tape", "build-documentation.yml")
+        add_row("lincc-frameworks", "tape", "smoke-test.yml")
+        add_row("lincc-frameworks", "tape", "testing-and-coverage.yml")
+
+        # Write postamble
+        file_out.write("</table>")
+        file_out.write(f"<p>Last Updated {datetime.now(tz).strftime('%H:%M %b %d, %y')}</p>")
+        file_out.write("</body></html>")
+
+
+if __name__ == "__main__":
+    token = sys.argv[1]
+    tz = pytz.timezone("America/New_York")
+    update_html(token, tz)
