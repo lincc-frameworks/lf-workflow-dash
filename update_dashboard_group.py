dce820b571aef818aa30aec6f83f117fdc492df1
delucchi-cmu <delucchi@andrew.cmu.edu>
2023-11-22 14:49:01 -0500

    OH NO MY SECRETS

--- update_dashboard_group.py
+++ update_dashboard_group.py
@@ -6,7 +6,6 @@ from lf_workflow_dash.update_dashboard import do_the_work
 
 if __name__ == "__main__":
     TOKEN = sys.argv[1]
-    TOKEN = "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"
     DATA_FILE = sys.argv[2]
     OUT_FILE = sys.argv[3]
     do_the_work(TOKEN, DATA_FILE, OUT_FILE)
