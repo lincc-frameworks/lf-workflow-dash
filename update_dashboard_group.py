f383fa7768083dc8f0601ea43107ab786a2ec66b
Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>
2024-01-11 13:14:33 -0500

    Remove v1 html generation. (#28)

--- update_dashboard_group.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from lf_workflow_dash.update_dashboard import do_the_work
-
-if __name__ == "__main__":
-    TOKEN = sys.argv[1]
-    DATA_FILE = sys.argv[2]
-    OUT_FILE = sys.argv[3]
-    do_the_work(TOKEN, DATA_FILE, OUT_FILE)
