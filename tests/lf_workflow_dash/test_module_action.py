e9ec13deebf1c658c9c1a004883bc2c65727d27f
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2025-09-23 10:01:52 -0400

    Get some more test coverage.

--- tests/lf_workflow_dash/test_module_action.py
+++ tests/lf_workflow_dash/test_module_action.py
@@ -4,6 +4,7 @@ import pytest
 
 from lf_workflow_dash.data_types import read_yaml_file
 from lf_workflow_dash.update_dashboard import update_html
+from lf_workflow_dash.github_request import get_conclusion_time
 
 
 @pytest.mark.parametrize(
@@ -12,9 +13,16 @@ from lf_workflow_dash.update_dashboard import update_html
         ("config/tracked_workflows.yaml", "lincc_output.html"),
         ("config/rail_tracked_workflows.yaml", "rail_output.html"),
         ("config/tracked_incubator.yaml", "incubator_output.html"),
+        ("config/lsdb_workflows.yaml", "lsdb_output.html"),
     ],
 )
 def test_do_the_work(datafile, outfile, tmp_path):
     output_path = os.path.join(tmp_path, outfile)
     context = read_yaml_file(datafile)
     update_html(output_path, context)
+
+
+def test_get_conclusion_time():
+    time, stale = get_conclusion_time({"updated_at": "2024-09-23T06:52:27Z"})
+    assert stale
+    assert time == "02:52<br>09/23/24"
