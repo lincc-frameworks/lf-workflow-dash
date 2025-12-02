2f0be9d82d587a8949d6ec5c01d40832190708c2
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2025-12-02 15:51:02 -0500

    Add reviewers to display

--- src/lf_workflow_dash/lsdb_interrupts/open_prs.py
+++ src/lf_workflow_dash/lsdb_interrupts/open_prs.py
@@ -26,18 +26,28 @@ def get_open_prs(org: str, repos: List[str], token: str) -> List[Dict]:
             url = f"{GITHUB_API_BASE}/repos/{org}/{repo}/pulls?state=open&per_page=100"
             pr_data = paginate_github_api(session, url)
 
-            prs = [
-                {
-                    "number": pr["number"],
-                    "title": pr["title"],
-                    "author": pr["user"],
-                    "updatedAt": pr["updated_at"],
-                    "url": pr["html_url"],
-                    "repo": repo,
-                    "is_draft": pr["draft"],
-                }
-                for pr in pr_data
-            ]
+            prs = []
+            for pr in pr_data:
+                author = pr["user"]["login"] if pr["user"] else "unknown"
+
+                # Reviewers are not returned in the pull request list response. Make a new request.
+                url = f"{GITHUB_API_BASE}/repos/{org}/{repo}/pulls/{pr['number']}/reviews"
+                reviewers = paginate_github_api(session, url)
+                reviewers = set(person["user"]["login"] for person in reviewers) - set([author])
+                list(reviewers).sort()
+
+                prs.append(
+                    {
+                        "number": pr["number"],
+                        "title": pr["title"],
+                        "author": pr["user"],
+                        "updatedAt": pr["updated_at"],
+                        "url": pr["html_url"],
+                        "repo": repo,
+                        "is_draft": pr["draft"],
+                        "reviewers": reviewers,
+                    }
+                )
             print(f"     Found {len(prs)} pull requests.")
 
             all_prs.extend(prs)
@@ -47,30 +57,56 @@ def get_open_prs(org: str, repos: List[str], token: str) -> List[Dict]:
     return all_prs
 
 
-def write_html_prs(prs: List[Dict], html_file: str):
+def write_html_prs(prs: List[Dict], html_file: str, page_title: str = "Pull Requests"):
     """Fill in the jinja template, using the prs found"""
     print(f"Writing HTML output to {html_file} ...")
 
+    prs.sort(key=lambda x: x["updatedAt"], reverse=True)
     now = datetime.now(timezone.utc)
     pr_summaries = []
+    reviewer_summaries = []
     for pr in prs:
         author = pr["author"]["login"] if pr["author"] else "unknown"
+        updated_at = get_humanized_updated_at(pr["updatedAt"], now)
+        tidy_title = pr["title"].replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
         pr_summaries.append(
             {
-                "updated_human": get_humanized_updated_at(pr["updatedAt"], now),
+                "updated_human": updated_at,
                 "author": author,
-                "title": pr["title"].replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;"),
+                "title": tidy_title,
                 "url": pr["url"],
                 "repo": pr["repo"],
                 "is_draft": "DRAFT" if pr["is_draft"] else "",
                 "is_bot": author in ["dependabot[bot]", "Copilot"],
+                "reviewers": ", ".join(pr["reviewers"]),
             }
         )
+        reviewer_summaries.extend(
+            [
+                {
+                    "updated_human": updated_at,
+                    "author": author,
+                    "title": tidy_title,
+                    "url": pr["url"],
+                    "repo": pr["repo"],
+                    "reviewer": reviewer,
+                }
+                for reviewer in pr["reviewers"]
+            ]
+        )
+
+    reviewer_summaries.sort(key=lambda x: x["reviewer"])
 
     environment = Environment(loader=FileSystemLoader("templates/"))
     template = environment.get_template("pr_list.jinja")
+    attributes = {
+        "page_title": page_title,
+        "all_prs": pr_summaries,
+        "reviewer_prs": reviewer_summaries,
+        "num_results": len(pr_summaries),
+    }
     with open(html_file, mode="w", encoding="utf-8") as results:
-        results.write(template.render({"all_prs": pr_summaries, "num_results": len(pr_summaries)}))
+        results.write(template.render(attributes))
     print(f"HTML output written to {html_file}")
 
 
@@ -78,13 +114,11 @@ def main(token):
     """Convenience method to do the work."""
     repos = get_lsdb_repos(token)
     prs = get_open_prs("astronomy-commons", repos, token)
-    prs.sort(key=lambda x: x["updatedAt"], reverse=True)
-    write_html_prs(prs, "html/lsdb_prs.html")
+    write_html_prs(prs, "html/lsdb_prs.html", "LSDB PRs")
 
     repos = get_org_repos("lincc-frameworks", token)
     prs = get_open_prs("lincc-frameworks", repos, token)
-    prs.sort(key=lambda x: x["updatedAt"], reverse=True)
-    write_html_prs(prs, "html/lincc_prs.html")
+    write_html_prs(prs, "html/lincc_prs.html", "lincc-frameworks PRs")
 
 
 if __name__ == "__main__":
